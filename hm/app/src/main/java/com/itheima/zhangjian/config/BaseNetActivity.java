a3506889bd340f2b68338526e0462815e60998b1
ZHANGJ <zhangjian1@itcast.cn>
2016-07-14 11:34:43 +0800

    剔除冗余代码

--- hm/app/src/main/java/com/itheima/zhangjian/config/BaseNetActivity.java
+++ hm/app/src/main/java/com/itheima/zhangjian/config/BaseNetActivity.java
@@ -1,12 +1,8 @@
 package com.itheima.zhangjian.config;
 
-import android.os.Bundle;
 import android.widget.Toast;
 
-import com.itheima.zhangjian.network.callback.RequestCallback;
 import com.itheima.zhangjian.network.callback.ResposeCallback;
-import com.itheima.zhangjian.network.factory.BaseManagerFactory;
-import com.itheima.zhangjian.network.neturl.BaseManagerType;
 
 /**
  * 作者:ZHANGJIAN  on 2016/7/14.
@@ -14,21 +10,6 @@ import com.itheima.zhangjian.network.neturl.BaseManagerType;
  * 说明:网络交互基类
  */
 public abstract class BaseNetActivity extends BaseDataActvitity implements ResposeCallback,NetWorkImpl{
-
-    public RequestCallback getManager(BaseManagerType type){
-        return  BaseManagerFactory.getInstance(type);
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
-    public void afterOnCreate(Bundle bundle) {
-
-    }
 
     @Override
     public void onNetworkError(int actionkey) {
--- hm/app/src/main/java/com/itheima/zhangjian/config/NetWorkImpl.java
+++ hm/app/src/main/java/com/itheima/zhangjian/config/NetWorkImpl.java
@@ -1,7 +1,5 @@
 package com.itheima.zhangjian.config;
 
-import com.itheima.zhangjian.network.manager.BaseManager;
-
 /**
  * 作者:ZHANGJIAN  on 2016/7/14.
  * 邮箱:zhangjian1@itcast.cn
